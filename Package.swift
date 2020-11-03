63b308bdebc51c26618da6100713caadd5ea8a80
Fabio Tacke <fabio@tacke.berlin>
2020-11-03 16:42:25 +0100

    Update Sodium SPM dependency

--- Package.swift
+++ Package.swift
@@ -11,8 +11,8 @@ let package = Package(
             targets: ["DoubleRatchet"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/jedisct1/swift-sodium", from: "0.8.0"),
-        .package(url: "https://github.com/TICESoftware/HKDF.git", from: "1.0.4"),
+        .package(url: "https://github.com/jedisct1/swift-sodium", from: "0.9.0"),
+        .package(url: "https://github.com/TICESoftware/HKDF.git", from: "1.0.8"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
     ],
     targets: [
