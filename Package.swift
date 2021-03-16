fabe285787d5d42eb7d055c7b060ec9dab6a3ef1
Fabio Tacke <f.tacke@ticeapp.com>
2021-03-16 14:58:37 +0100

    Use Sodium with SPM workaround

--- Package.swift
+++ Package.swift
@@ -11,8 +11,8 @@ let package = Package(
             targets: ["DoubleRatchet"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/jedisct1/swift-sodium.git", from: "0.9.1"),
-        .package(url: "https://github.com/TICESoftware/HKDF.git", from: "1.0.9"),
+        .package(url: "https://github.com/TICESoftware/swift-sodium.git", from: "0.9.2"),
+        .package(url: "https://github.com/TICESoftware/HKDF.git", from: "1.0.10"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
     ],
     targets: [
